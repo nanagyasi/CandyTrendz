7d2d9a9d2a38bd3381b752c250e0344299dd2b9c
nanagyasi <nanagyasi.ak@gmail.com>
2023-11-01 14:57:14 +0000

    swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
     cssMode: true,
+    loop: true,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
     navigation: {
         nextE1: ".swiper-button-next",
         prevE1: ".swiper-button-prev",
